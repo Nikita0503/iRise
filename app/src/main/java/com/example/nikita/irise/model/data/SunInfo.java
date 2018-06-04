a9c3fe5031f2716eb3925e47a96ddf782e9783c4
nikita0503 <nikita0503ua@gmail.com>
2018-06-04 17:32:45 +0300

    Sunrise-sunset application.

--- app/src/main/java/com/example/nikita/irise/model/data/SunInfo.java
+++ app/src/main/java/com/example/nikita/irise/model/data/SunInfo.java
@@ -9,24 +9,9 @@ public class SunInfo {
     @SerializedName("results")
     @Expose
     private Results results;
-    @SerializedName("status")
-    @Expose
-    private String status;
 
     public Results getResults() {
         return results;
-    }
-
-    public void setResults(Results results) {
-        this.results = results;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
     }
 
 }
